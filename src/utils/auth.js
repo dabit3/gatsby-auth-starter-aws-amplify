919cc53846a098b538577662c90534936a561003
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2018-08-26 15:20:36 -0700

    reorganized location of function

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,11 +1,11 @@
 const isBrowser = typeof window !== `undefined`
 
+export const setUser = user => (window.localStorage.gatsbyUser = JSON.stringify(user))
+
 const getUser = () =>
   window.localStorage.gatsbyUser
     ? JSON.parse(window.localStorage.gatsbyUser)
     : {}
-
-export const setUser = user => (window.localStorage.gatsbyUser = JSON.stringify(user))
 
 export const isLoggedIn = () => {
   if (!isBrowser) return false

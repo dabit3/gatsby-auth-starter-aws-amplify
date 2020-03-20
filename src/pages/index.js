007c146454a7ad24764c211aa637dd4328963723
grzegorz.herbowicz <grzegorz.herbowicz@externals.adidas.com>
2020-03-20 22:53:31 +0100

    hide login button when logged in

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 
+import { isLoggedIn } from "../utils/auth"
+
 import Amplify from 'aws-amplify'
 import config from '../aws-exports'
 Amplify.configure(config)
@@ -11,8 +13,14 @@ const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site with multi-user authentication powered by <a href="https://amplify.aws">AWS Amplify</a></p>
-    <p>Create a new account: <Link to="/app/signup">Sign Up</Link></p>
-    <Link to="/app/login">Sign In</Link><br />
+    {
+      !isLoggedIn() && (
+        <>
+          <p>Create a new account: <Link to="/app/signup">Sign Up</Link></p>
+          <Link to="/app/login">Sign In</Link><br />
+        </>
+      )
+    }
     <Link to="/app/home">Home</Link><br />
     <Link to="/app/profile">Your profile</Link>
   </Layout>

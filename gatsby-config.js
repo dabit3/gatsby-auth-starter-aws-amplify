4ae4474f85ace4fe2a34989eb49fc0f7d31caee0
grzegorz.herbowicz <grzegorz.herbowicz@externals.adidas.com>
2020-03-20 18:40:16 +0100

    ci test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `d3.vc`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
